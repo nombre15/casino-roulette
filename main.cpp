61c37925affa65ac945cfacb14a58964bf4b8465
nombre15 <81451283+nombre15@users.noreply.github.com>
2021-05-22 12:44:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,14 +6,17 @@
 using namespace std;
 
 void NUMBER (int);
+void SELECT_NUMBER(int);
 
 int main()
 {
     string bet;
     int BetNumber;
+    int number = rand() % 37;
     cout << "how do you want to bet" << endl;
     cout << " odds \n even \n red \n black \n single number \n number from 1 - 18 \n number from 19 - 36" << endl;
     cin >> bet;
+    srand(time(0));
 
     single:
     if(bet == "single"){
@@ -25,33 +28,27 @@ int main()
         goto single;
     }
 
-    srand(time(0));
+    SELECT_NUMBER(number);
 
-    cout <<"spinning..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
+    int number = rand() % 37;
+    if(number == BetNumber){
 
-        int number = rand() % 37;
-        if(number == BetNumber){
+        cout << "\nyou win!" << endl;
+        cout << "the number was ";
+        NUMBER(number);
 
-            cout << "\nyou win!" << endl;
-            cout << "the number was ";
-            NUMBER(number);
+  }
+    else if(number != BetNumber){
 
-        }
-        else if(number != BetNumber){
+        cout << "\nyou lose!" << endl;
+        cout << "the number was ";
+        NUMBER(number);
+     }
+  }
 
-            cout << "\nyou lose!" << endl;
-            cout << "the number was ";
-            NUMBER(number);
-        }
-    }
+    if(bet == "odds" || bet == "odd"){
 
-     if(bet == "odds" || bet == "odd"){
-
-        srand(time(0));
-        cout <<"spinning..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
-        int number = rand() % 37;
+        SELECT_NUMBER(number);
 
         if (number % 2 != 0){
 
@@ -65,13 +62,10 @@ int main()
             cout << "the number was ";
             NUMBER(number);
         }
-     }
+  }
      if(bet == "evens" || bet == "even"){
 
-        srand(time(0));
-        cout <<"spinning..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
-        int number = rand() % 37;
+       SELECT_NUMBER(number);
 
         if (number % 2 == 0){
 
@@ -85,13 +79,10 @@ int main()
             cout << "the number was ";
             NUMBER(number);
         }
-     }
+  }
      if(bet == "red"){
 
-        srand(time(0));
-        cout <<"spinning..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
-        int number = rand() % 37;
+        SELECT_NUMBER(number);
 
         if (number % 2 == 0  && ((11 < number && number < 19) || (29 < number && number < 37)))
         {
@@ -111,15 +102,12 @@ int main()
            cout << "the number was ";
            NUMBER(number);
         }
-     }
+  }
      if(bet == "black"){
 
-        srand(time(0));
-        cout <<"spinning..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
-        int number = rand() % 37;
+        SELECT_NUMBER(number);
 
-      if (number % 2 == 0 && ((0 < number && number < 11) || (19 < number && number < 29)))
+       if (number % 2 == 0 && ((0 < number && number < 11) || (19 < number && number < 29)))
         {
             cout << "\nwinner winner" << endl;
             cout << "the number was ";
@@ -131,20 +119,17 @@ int main()
             cout << "the number was ";
             NUMBER(number);
         }
-    else{
+      else{
 
         cout << "\nyou loseee" << endl;
            cout << "the number was ";
            NUMBER(number);
         }
-    }
+  }
 
     if(bet == "1-18" || bet == "number from 1 - 18" || bet == "1to18" || bet == "number from 1 to 18"){
 
-        srand(time(0));
-        cout <<"spinning..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
-        int number = rand() % 37;
+        SELECT_NUMBER(number);
 
         if(0 < number && number < 19){
 
@@ -152,19 +137,16 @@ int main()
             cout << "the number was ";
             NUMBER(number);
         }
-        else{
+      else{
 
             cout << "\nyou lose!" << endl;
             cout << "the number was ";
             NUMBER(number);
         }
-    }
+  }
     if(bet == "19-36" || bet == "number from 19 - 36" || bet == "19to36" || bet == "number from 19 to 36"){
 
-        srand(time(0));
-        cout <<"spinning..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
-        int number = rand() % 37;
+        SELECT_NUMBER(number);
 
         if(18 < number && number < 37){
 
@@ -179,44 +161,47 @@ int main()
             NUMBER(number);
         }
     }
-
 }
 
+//choose color according to the number
 void NUMBER(int number){
+
  if(number == 0)
-        {
-            cout << number << " green" << endl;
-        }
+{
+    cout << number << " green" << endl; //GREEN
+}
 
-        /*even blacks (1 -> 10 & 20 -> 28)*/
-        else if (number % 2 == 0 && ((0 < number && number < 11) || (19 < number && number < 29)))
+        else if (number % 2 == 0 && ((0 < number && number < 11) || (19 < number && number < 29))) //even blacks (1 -> 10 & 20 -> 28)
         {
             cout << number << " black" << endl;
 
         }
 
-        /*even reds (12 -> 18 & 30 -> 36)*/
-        else if (number % 2 == 0 && ((11 < number && number < 19) || (29 < number && number < 37)))
+        else if (number % 2 == 0 && ((11 < number && number < 19) || (29 < number && number < 37))) //even reds (12 -> 18 & 30 -> 36)
         {
             cout << number << " red" << endl;
         }
 
-        /*odd reds (1 -> 9 & 19 -> 27)*/
-        else if (number % 2 != 0 && ((0 < number && number < 10) || (18 < number && number < 28)))
+        else if (number % 2 != 0 && ((0 < number && number < 10) || (18 < number && number < 28))) //odd reds (1 -> 9 & 19 -> 27)
         {
             cout << number << " red" << endl;
         }
 
-        /*odd blacks (11 -> 17 & 29 -> 35)*/
-        else if (number % 2 != 0 && ((10 < number && number < 18) || (28 < number && number < 36)))
+        else if (number % 2 != 0 && ((10 < number && number < 18) || (28 < number && number < 36))) //odd blacks (11 -> 17 & 29 -> 35)
         {
             cout << number << " black" << endl;
         }
 
-        /*error?*/
         else
         {
-            cout << number << "what the fuck error???" << endl;
+            cout << number << "what the fuck error???" << endl; //error?
         }
 }
 
+//select random number
+void SELECT_NUMBER(int number){
+
+     number = rand() % 37;
+     cout <<"spinning..." << endl;
+     this_thread::sleep_for (chrono::seconds(2));
+}
